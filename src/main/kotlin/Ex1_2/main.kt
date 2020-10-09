d14b3a8acd23ddda00eaa64e132260732ee0860d
Kirill Novikov <kolbasa712@yandex.ru>
2020-10-09 20:15:21 +0500

    Задание 2

--- src/main/kotlin/Ex1_2/main.kt
+++ src/main/kotlin/Ex1_2/main.kt
@@ -1,5 +1,16 @@
 package Ex1_2
 
 fun main(){
+    println("Введите число лайков")
+    var likes = readLine()!!.toInt()
 
+    likesFun(likes)
+}
+
+fun likesFun(likes : Int) {
+    if ( ((likes % 10) == 1) && ((likes !== 11) && (likes % 10 != 11))){
+        print("Понравилось " + likes + " человеку.")
+    }else if (((likes % 10 >= 1) && (likes % 10 <= 9)) || (likes % 10 == 0)) {
+        print("Понравилось " + likes + " людям.")
+    }
 }
